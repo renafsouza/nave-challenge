db6ecf34dc2f82fe86190d4821d8a5c3289afe71
Renã Souza <40342520+renafsouza@users.noreply.github.com>
2020-02-28 14:05:55 -0300

    Use REST pattern for routes

--- desafio back-end/src/routes.js
+++ desafio back-end/src/routes.js
@@ -4,13 +4,13 @@ const commentController = require(`./controllers/comment`)
 const Express = require('express');
 const routes = Express.Router();
 
-routes.get('/posts/list', postController.list);
-routes.post('/posts/create', postController.create);
-routes.put('/posts/update', postController.update);
+routes.get('/posts', postController.list);
+routes.post('/posts', postController.create);
+routes.put('/posts', postController.update);
 
-routes.get('/comments/list', commentController.list);
-routes.post('/comments/create', commentController.create);
-routes.put('/comments/update', commentController.update);
+routes.get('/comments', commentController.list);
+routes.post('/comments', commentController.create);
+routes.put('/comments', commentController.update);
 
 // Export
 module.exports = routes;
